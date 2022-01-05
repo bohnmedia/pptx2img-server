4fa0ea653ed9ecca7af85666586acd2475611488
dennisbohn <dennis.bohn@bohnmedia.de>
2022-01-05 23:23:55 +0100

    testscript.php angepasst

--- testscript.php
+++ testscript.php
@@ -1,26 +1,33 @@
 <?php
 
-    $domain = "server.eintragen.de";
-	$inputFile = "/var/www/html/test/test.pptx";
-	$outputFile = "/var/www/html/test/test.jpg";
-    $format = "jpeg"; // optional: "jpeg" or "png" (server default: "jpeg")
-    $quality = 80;    // optional: only needed, when format is "jpeg" (server default: 80)
-    $dpi = 150;       // optional: (server default: 150)
-	$url = sprintf("https://%s/convert.php", $domain);
+	//Domain
+    $domain = "my.server.com";
 	
-	$pptx = new CURLFile($inputFile);
+	// Input and output file
+	$inputFile = "/var/www/html/test/input.pptx";
+	$outputFile = "/var/www/html/test/output.jpg";
 	
+	// PostFields
+	$postFields = ["pptx" => new CURLFile($inputFile)];
+	
+	// Additional settings (optional)
+    $postFields["format"] = "jpeg"; // optional: "jpeg" or "png" (server default: "jpeg")
+    $postFields["quality"] = 80;    // optional: only needed, when format is "jpeg" (server default: 80)
+    $postFields["dpi"] = 150;       // optional: (server default: 150)
+	
+	// Username and passwort (optional)
+    $username = "username";
+    $password = "password";
+	
+	// cURL request
 	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_URL, sprintf("https://%s/convert.php", $domain));
+    if (isset($username) && isset($password)) curl_setopt($ch, CURLOPT_USERPWD, $username . ":" . $password);
 	curl_setopt($ch, CURLOPT_POST, true);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, [
-        'pptx' => $pptx,
-        'format' => $format,   // optional
-        'quality' => $quality, // optional
-        'dpi' => $dpi          // optional
-    ]);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	$jpg = curl_exec($ch);
+	$imgData = curl_exec($ch);
 	curl_close($ch);
 	
-	file_put_contents($outputFile, $jpg);
+	// Write response to file
+	file_put_contents($outputFile, $imgData);
