9a0ca5616ce028e02485d479f186b8d238c6b6ba
dennisbohn <dennis.bohn@bohnmedia.de>
2022-01-05 23:28:10 +0100

    Code aufgehübscht

--- convert.php
+++ convert.php
@@ -1,53 +1,53 @@
 <?php
 
-	// Check for file
-	if (!isset($_FILES["pptx"]) || !$_FILES["pptx"]["tmp_name"]) {
-		http_response_code(400);
-		header("Content-Type: text/plain");
-		echo "ppxt file missing";
-		exit();
-	}
+    // Check for file
+    if (!isset($_FILES["pptx"]) || !$_FILES["pptx"]["tmp_name"]) {
+        http_response_code(400);
+        header("Content-Type: text/plain");
+        echo "ppxt file missing";
+        exit();
+    }
 
-	// Config
-	$inputFile = $_FILES["pptx"]["tmp_name"];
+    // Config
+    $inputFile = $_FILES["pptx"]["tmp_name"];
     $resolution = isset($_POST["resolution"]) ? $_POST["resolution"] : 150;
     $format = isset($_POST["format"]) ? $_POST["format"] : "jpeg";
     $quality = isset($_POST["quality"]) ? (int)$_POST["quality"] : 80;
     if ($format === "jpg") $format = "jpeg";
 
-	// Create unique tmp directory for file output
-	$tmpDir = sprintf('%s/pptx2img-%s', sys_get_temp_dir(), md5(mt_rand()));
-	mkdir($tmpDir);
+    // Create unique tmp directory for file output
+    $tmpDir = sprintf('%s/pptx2img-%s', sys_get_temp_dir(), md5(mt_rand()));
+    mkdir($tmpDir);
 
-	// Convert pptx to pdf
-	putenv('HOME=' . __DIR__);
-	exec("/usr/bin/libreoffice --headless --impress --convert-to pdf " . $inputFile . " --outdir " . $tmpDir);
+    // Convert pptx to pdf
+    putenv('HOME=' . __DIR__);
+    exec("/usr/bin/libreoffice --headless --impress --convert-to pdf " . $inputFile . " --outdir " . $tmpDir);
 
-	// Get filelist of output directory
-	$files = glob($tmpDir . '/*');
-	
-	// Convert pdf to jpeg
-	$outputFile = sprintf('%s/output.png', $tmpDir);
-	$image = new Imagick();
-	$image->setResolution($resolution, $resolution);
-	$image->readImage($files[0]);
-	$image->setImageFormat($format);
+    // Get filelist of output directory
+    $files = glob($tmpDir . '/*');
+
+    // Convert pdf to jpeg
+    $outputFile = sprintf('%s/output.png', $tmpDir);
+    $image = new Imagick();
+    $image->setResolution($resolution, $resolution);
+    $image->readImage($files[0]);
+    $image->setImageFormat($format);
     if ($format === "jpeg") {
         $image->setImageCompression(Imagick::COMPRESSION_JPEG);
-	    $image->setImageCompressionQuality($quality);
+        $image->setImageCompressionQuality($quality);
     }
-	$image->writeImage($outputFile);
-	$image->clear(); 
-	$image->destroy();
+    $image->writeImage($outputFile);
+    $image->clear(); 
+    $image->destroy();
 
-	// Output image
-	header('Content-Type: ' . mime_content_type($outputFile));
-	header('Content-Length: ' . filesize($outputFile));
-	readfile($outputFile);
-	
-	// Delete tmp directory
-	unlink($outputFile);
-	foreach ($files as $file) {
-		unlink($file);
-	}
-	rmdir($tmpDir);
+    // Output image
+    header('Content-Type: ' . mime_content_type($outputFile));
+    header('Content-Length: ' . filesize($outputFile));
+    readfile($outputFile);
+
+    // Delete tmp directory
+    unlink($outputFile);
+    foreach ($files as $file) {
+        unlink($file);
+    }
+    rmdir($tmpDir);
--- testscript.php
+++ testscript.php
@@ -1,33 +1,33 @@
 <?php
 
-	//Domain
+    //Domain
     $domain = "my.server.com";
-	
-	// Input and output file
-	$inputFile = "/var/www/html/test/input.pptx";
-	$outputFile = "/var/www/html/test/output.jpg";
-	
-	// PostFields
-	$postFields = ["pptx" => new CURLFile($inputFile)];
-	
-	// Additional settings (optional)
+
+    // Input and output file
+    $inputFile = "/var/www/html/test/input.pptx";
+    $outputFile = "/var/www/html/test/output.jpg";
+
+    // PostFields
+    $postFields = ["pptx" => new CURLFile($inputFile)];
+
+    // Additional settings (optional)
     $postFields["format"] = "jpeg"; // optional: "jpeg" or "png" (server default: "jpeg")
     $postFields["quality"] = 80;    // optional: only needed, when format is "jpeg" (server default: 80)
     $postFields["dpi"] = 150;       // optional: (server default: 150)
-	
-	// Username and passwort (optional)
+
+    // Username and passwort (optional)
     $username = "username";
     $password = "password";
-	
-	// cURL request
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, sprintf("https://%s/convert.php", $domain));
+
+    // cURL request
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, sprintf("https://%s/convert.php", $domain));
     if (isset($username) && isset($password)) curl_setopt($ch, CURLOPT_USERPWD, $username . ":" . $password);
-	curl_setopt($ch, CURLOPT_POST, true);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	$imgData = curl_exec($ch);
-	curl_close($ch);
-	
-	// Write response to file
-	file_put_contents($outputFile, $imgData);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $imgData = curl_exec($ch);
+    curl_close($ch);
+
+    // Write response to file
+    file_put_contents($outputFile, $imgData);
